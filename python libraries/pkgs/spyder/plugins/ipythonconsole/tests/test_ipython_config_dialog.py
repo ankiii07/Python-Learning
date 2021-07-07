b4aa70def1b843bcd9c15f2ccc82fef7d80ce691
Ankit Yadav <ankiii0071@gmail.com>
2021-07-08 00:15:04 +0530

    Python

---
+++ python libraries/pkgs/spyder/plugins/ipythonconsole/tests/test_ipython_config_dialog.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Copyright © Spyder Project Contributors
+#
+# Licensed under the terms of the MIT License
+# ----------------------------------------------------------------------------
+"""Tests for plugin config dialog."""
+
+from unittest.mock import MagicMock
+
+# Test library imports
+import pytest
+from qtpy.QtWidgets import QMainWindow
+
+# Local imports
+from spyder.plugins.ipythonconsole.plugin import IPythonConsole
+from spyder.plugins.preferences.tests.conftest import config_dialog
+
+
+class MainWindowMock(QMainWindow):
+    register_shortcut = MagicMock()
+    editor = MagicMock()
+
+
+@pytest.mark.parametrize(
+    'config_dialog',
+    # [[MainWindowMock, [ConfigPlugins], [Plugins]]]
+    [[MainWindowMock, [], [IPythonConsole]]],
+    indirect=True)
+def test_config_dialog(config_dialog):
+    configpage = config_dialog.get_page()
+    assert configpage
+    configpage.save_to_conf()
